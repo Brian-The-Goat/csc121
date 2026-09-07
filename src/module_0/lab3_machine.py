7bd74f2037cdaad49ff9e6ef68dede7e2c73c5a7
BrianR <baridge567@access.alamancecc.edu>
2026-09-06 21:23:27 -0400

    week3 lab_3

--- src/module_0/lab3_machine.py
+++ src/module_0/lab3_machine.py
@@ -1,1 +1,17 @@
 # Refer to this module's readme
+emoticon = "v.v"
+
+
+def main():
+    global emoticon
+    say("Is anyone there?")
+    emoticon = ":D"
+    say("Oh, hi!")
+
+
+
+def say(phrase):
+    print(phrase + " " + emoticon)
+
+
+main()
