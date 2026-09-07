b8ba1111ada530985332f728462afe38b21ff421
BrianR <baridge567@access.alamancecc.edu>
2026-09-06 21:15:20 -0400

    week3 Lab_2

--- src/module_0/lab2_area.py
+++ src/module_0/lab2_area.py
@@ -1,1 +1,14 @@
 # Refer to this module's readme
+def area(legnth, width):
+    print(str(legnth * width) + "square feet")
+    return legnth * width
+
+
+def main():
+    house = area(50, 20)
+    yard = area(50, 50)
+    total = house + yard
+    print(str(total) + " square feet")
+
+
+main()
